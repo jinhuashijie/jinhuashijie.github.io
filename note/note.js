566d6e5420385bda45636ba31c2260a7541334bf
金花世界 <947575519@qq.com>
2018-05-08 10:23:21 +0800

    1

--- note/note.js
+++ note/note.js
@@ -12,6 +12,8 @@
 			  平板：大于768px；  (min-width: 767px) and (max-width: 1129px)
 			  手机 ：小于480px； (max-width: 500px)
 4：html-css-js-jquery-php-mysql-nodejs-es6-git-(angular react  veu) -webpack-react router-web安全--动画
+	关键是要能够利用这些东西作出各种效果来才行
+                               /*            css知识点开始                               */
 知识点：1：响应式布局，已经解决，
 	2：css动画，画图；
 	动画：百分比和from-to效果是一样的；animation-delay:2s;  -direction:normal/forwards（保持最后状态）
@@ -32,4 +34,60 @@
 	perspective-orgin:
 	backface-visibility:设置背面是否可见，visibilitey  hidden  就是元素透不透明，旋转之后还能看到后面
 7：html引用php文件 <?php include 'footer.php';?>
+8:纯粹css修改复选框的样式是可以的，主要利用label和伪元素，以及动画
+	1:label里面for的是input对应 id；
+	2：原来的选框被隐藏了
+	3：利用span，以及伪元素新增加了一个形状
+	4：当checked处于被选中时，触发了一个事件
+	5:使用input[type="checkbox"]=checked是可以的
+	6：最终证明简单的是可以的
+	7:伪元素必须设置inline-block，要不然宽高失效
+
+                      /*             jquery部分              */
+1：jquery分页插件
+	1：在逻辑架构上
+		1：每循环一次，增加一次ul，然后判断ul的高度，小于预设值就往里面增加li
+		2：大于ul的高度，就往页面的特定盒子里面增加一个按钮；同时设置其他的ul隐藏起来
+	2：遇到的问题
+		1：在最外面需要一个初始值，用于记录已经增加了多少个li
+		2：在最外层的循环里必须让i的数值小于数组总长度，而这个总长度是余下的，
+		3：不仅仅要小于剩下的，i的本身是增长的，所以需要另一个值把i值增加到剩余长度里，抵消i的增长；
+		4：在ajax末尾用delegate的方法操作动态元素是没有问题的。
+		5：在ajax末尾操作动态元素的属性也是没问题的；前面报错是自己写的问题
+		6：在append方法后面是不能进行运算的，要把运算放在前面才行，
+	3：类名指向解释：
+		1：msg是所有ul的父元素，是最外面的容器
+		2： btn是新增的按钮的父元素，
+		3：data是ajax从PHP拿回来的数据，里面的名字要改成自己的；
+	var len=data.length;
+	var ulx=0;
+	var x=0;
+	for(i=0;i<len-ulx+x;i++){
+		$(".msg").append("<ul></ul>");
+		for(s=0;s<12;s++){
+			var h=parseInt($(".msg ul").eq(i).css("height"));
+			console.log(h);
+			if(h>85){
+				if(s+ulx<=len){
+					$(".msg ul").not($(".msg ul").eq(0)).css("display","none");
+					var n=i+1;
+					$(".btn").append("<button>"+n+"</button>");
+					x=i;
+					ulx=ulx+s;
+					break;
+				}
+			}else{//顺序错了就会报错；不应该是顺序的问题；
+				$(".msg ul").eq(i).append("<li>"+data[s+ulx].msg+"</li>");
+			}
+		}
+		
+	}
+2：<a href="javascript:;">  a标签取消默认行为
+
+
+
+
+
+
+	/*            css知识点结束                               */
 
