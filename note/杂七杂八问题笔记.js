a97cceb43c4436a375d8bd40ea0a104764736cc0
金花世界 <947575519@qq.com>
2018-05-28 09:30:53 +0800

    杂七杂八问题笔记

---
+++ note/杂七杂八问题笔记.js
@@ -0,0 +1,27 @@
+1:var h=document.getElementsByClassName('headul0');
+	console.log(h);
+	h[0].style.display="block";
+原生js获取class得到的是一个数组；
+onclick="soud();return false;"javascript 组织默认事件，
+e.preventDefault()  方法为什么不行？return false 也不行
+onclick="soud(e);"  加上e也没用,怎么都不行，只有后面加return false
+完点击函数完全写在js里面的时候还能起作用；
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
