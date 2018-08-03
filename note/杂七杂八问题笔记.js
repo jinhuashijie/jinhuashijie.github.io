70f89bc2cd00f9919da8b38ee4b11dd95550cae5
金花世界 <947575519@qq.com>
2018-08-03 13:49:18 +0800

    最近老是连不上啊，高墙大院好难翻啊

--- note/杂七杂八问题笔记.js
+++ note/杂七杂八问题笔记.js
@@ -1,4 +1,4 @@
-1:var h=document.getElementsByClassName('headul0');
+﻿1:var h=document.getElementsByClassName('headul0');
 	console.log(h);
 	h[0].style.display="block";
 原生js获取class得到的是一个数组；
@@ -6,6 +6,7 @@ onclick="soud();return false;"javascript 组织默认事件，
 e.preventDefault()  方法为什么不行？return false 也不行
 onclick="soud(e);"  加上e也没用,怎么都不行，只有后面加return false
 完点击函数完全写在js里面的时候还能起作用；
+2：还是需要三级
 
 
 
